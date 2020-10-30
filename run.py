567da5b6111e808d816a6c6a6835260201612078
paner28 <toshimichi23masa@gmail.com>
2020-10-30 14:46:56 +0900

    second commit

---
+++ run.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello World!"
+    
+@app.route('/flask')
+def hello_sample():
+    return "Hello Flask."
+
+@app.route('/user/<user_id>')
+def hello_person(user_id):
+    return "Hello " + user_id
+    
+if __name__ == '__main__':
+    app.run()
