e8bcbdb155a8cf7b7a6e6f851c0b91ae48b76b62
paner28 <toshimichi23masa@gmail.com>
2020-10-30 16:17:59 +0900

    second commit

--- run.py
+++ run.py
@@ -4,6 +4,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return "Hello World!"
+    
+@app.route('/flask')
+def hello_sample():
+    return "Hello Flask."
 
+@app.route('/user/<user_id>')
+def hello_person(user_id):
+    return "Hello " + user_id
+    
 if __name__ == '__main__':
     app.run()
